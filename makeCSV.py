f49111314134433dde8b6f36f5d4252d37496d5f
marco <nerdinary@gmail.com>
2016-10-05 21:57:30 +0900

    modify makeCSV.py

--- makeCSV.py
+++ makeCSV.py
@@ -9,20 +9,21 @@ else:
 def wholeData():
     db=client.research
     sillokFirstEmergence=db.sillokFirstEmergence
-    fieldnames= ['계열','이름','생년','졸년','본관','왕명','합격년도','합격연령','순위','응시인원','시험유형','실록최초등장']
+    fieldnames= ['계열','level','이름','생년','졸년','본관','왕명','합격년도','합격연령','순위','응시인원','시험유형','실록최초등장','gap']
     with open('FirstEmergence.csv','w') as csvfile:
         writer=csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+        writer.writeheader()
         d=dict()
         for i in fieldnames:
             d[i]=""
         for i in sillokFirstEmergence.find():
             empty_d=d
+            empty_d['gap'] = int(i['실록최초등장'])-int(i['합격년도'])
             keys=list(i.keys())
             for key in keys:
                 if key in fieldnames:
                     empty_d[key] = i[key]
             full_d=empty_d
-            print(full_d)
+
             writer.writerow(full_d)
 wholeData()
