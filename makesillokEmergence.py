a2bfe77865d499446832958c0b1271c596a776f1
nerdinary1 <nerdinary@gmail.com>
2016-10-05 10:55:44 +0900

    new start

--- makesillokEmergence.py
+++ makesillokEmergence.py
@@ -23,13 +23,24 @@ def makeFirstEmergence():
         keys=list(man.keys())
 
         if "url" not in keys:
-            continue
+            sillokFirstEmergence.insert(man)
+            sillokFirstEmergence.update({"_id": man['_id']}, {"$set": {"실록최초등장": 0}})
 
         #살면서 실록에 기록된 관직이 한 번도 없는 경우에는 처음 실록에 등장한 날짜를 insert
         elif "ref" not in keys:
+        # else:
+            passyear = man['합격년도']
+
             records=[i['date'] for i in sillokIntegrated.find({"nameIndex":{"$in":[man['nameIndex']]}})]
             records.sort()
-            firstEmergence=records[0].split('-')[0]
+            for record in records:
+                if int(record.split('-')[0])<int(passyear):
+                    temp=records.pop()
+            try:
+                firstEmergence=records[0].split('-')[0]
+
+            except:
+                firstEmergence=temp.split('-')[0]
             sillokFirstEmergence.insert(man)
             sillokFirstEmergence.update({"_id": man['_id']}, {"$set": {"실록최초등장": firstEmergence}})
 
@@ -37,13 +48,43 @@ def makeFirstEmergence():
 
         #실록에 기록된 관직이 한 번이라도 있었다면, 그 관직명으로 실록에 기록된 날짜를 insert
         else:
-            firstId=man['ref'][0]
-            firstEmergence=[i for i in sillokManInfo.find({"_id":firstId})].pop()
+            passyear= man['합격년도']
+            idx=0
+            # while True:
+            #     try:
+            #         t=man['ref'][idx]
+            #         if sillokManInfo.find_one({"_id":t})['년']>=passyear:
+            #             break
+            #         idx+=1
+            #     except:
+            #         idx=0
+            #         break
+            #
+            # firstId=man['ref'][idx]
+            # firstEmergence=[i for i in sillokManInfo.find({"_id":firstId})].pop()
+
+
+            records = [i['date'] for i in sillokIntegrated.find({"nameIndex": {"$in": [man['nameIndex']]}})]
+            records.sort()
+
+            for record in records:
+
+                if int(record.split('-')[0]) <= int(passyear):
+                    temp = records.pop(0)
+
+
+            try:
+                firstEmergence = records[0].split('-')[0]
+
+
+            except:
+                firstEmergence = temp.split('-')[0]
+
             # if '윤' in str(firstEmergence['월']):
             #     firstEmergence=str(firstEmergence['년'])+"-"+ firstEmergence['월'].replace('윤','')+str(firstEmergence['일'])+"L1"
             # else:
             #     firstEmergence = str(firstEmergence['년']) + "-" + str(firstEmergence['월'])+"-"+str(firstEmergence['일']) + "L0"
-            firstEmergence=firstEmergence['년']
+            # firstEmergence=firstEmergence['년']
             sillokFirstEmergence.insert(man)
             sillokFirstEmergence.update({"_id":man['_id']}, {"$set":{"실록최초등장":firstEmergence}})
 
